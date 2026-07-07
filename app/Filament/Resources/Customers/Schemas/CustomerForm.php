cbc887955ac2a3bcb4f3fa6e00311d346c0bd0ff
ajaypoonia3012 <ajaypoonia3012@gmail.com>
2026-07-07 18:12:23 -0400

    Fix Customer resource schema imports for Filament v4

--- app/Filament/Resources/Customers/Schemas/CustomerForm.php
+++ app/Filament/Resources/Customers/Schemas/CustomerForm.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\Customers\Schemas;
 use App\Models\Customer;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Grid;
+use Filament\Schemas\Components\Grid;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
